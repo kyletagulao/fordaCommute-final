a20fa65586b8f8db5a6f59a4d6ba484465418241
kyletagulao <20-03001@g.batstate-u.edu.ph>
2022-11-21 18:31:56 +0800

    Map and Search Bar

--- app/src/main/java/com/org/fordacommute/MainActivity.kt
+++ app/src/main/java/com/org/fordacommute/MainActivity.kt
@@ -2,12 +2,33 @@ package com.org.fordacommute
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.libraries.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity()  {
+
+    lateinit var mapFragment: SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+//        mapFragment = supportFragmentManager.findFragmentById(R.id.maps) as SupportMapFragment
+//        mapFragment.getMapAsync(OnMapReadyCallback { this })
+
+//        mapFragment.getMapAsync(OnMapReadyCallback {
+//            googleMap = googleMap
+//        })
+
+//        val layer = KmlLayer(googleMap, R.raw.bp01, applicationContext)
+//        layer.addLayerToMap()
+
     }
+
 
     override fun onBackPressed() {
         //super.onBackPressed()
